724a2656eab91bfa17d843576df1e8f8af9699c6
Fayol Tchamba <fayoltchamba@gmail.com>
2024-09-30 00:13:17 +0100

    Mise de tous ces fichiers dans un dossier

--- Phase-1/Moyenne-de-7-nombres.c
+++ Phase-1/Moyenne-de-7-nombres.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    float a=0, b=0, c=0, d=0,e=0, f=0,g=0, moyenne =0;
-    printf("Entrez 07 nombres de votre choix :\n");
-    scanf("%f%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f,&g);
-    moyenne=(a+b+c+d+e+f+g)/7;
-    printf("La moyenne de vos nombres est : %.2f",moyenne);
-    return 0;
+     float a = 0, b = 0, c = 0, d = 0,e = 0, f = 0,g = 0, moyenne = 0;
+     printf("Entrez 07 nombres de votre choix :\n");
+     scanf("%f%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f,&g);
+     moyenne = (a + b + c + d + e + f + g)/ 7;
+     printf("La moyenne de vos nombres est : %.2f",moyenne);
+     return 0;
 }
